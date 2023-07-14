18ce19c967522775c5c43facbed109b59b8b3e1f
Sanjeevi20 <136660773+Sanjeevi20@users.noreply.github.com>
2023-07-14 11:21:25 +0530

    Create String - Concatenate.java

---
+++ String - Concatenate.java
@@ -0,0 +1,28 @@
+There were two best friends. They had a fight between them and they were not talked with each other. Help those two peoples to join again and let them enjoyed. Find the concatenation of given two strings using string library functions:
+
+Input Format:
+
+Input consist of 2 string.
+Sample Input & Output:
+
+Hello
+
+all
+
+Helloall
+
+
+
+Code:
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String san=sc.nextLine();
+        String son=sc.nextLine();
+        System.out.println(san+son);
+    }
+}
