0ec7e69c0f643518f058d9a5c2892ab1cdca5b24
Sanjeevi20 <136660773+Sanjeevi20@users.noreply.github.com>
2023-07-14 11:36:46 +0530

    Create Class and Object - 1.java

---
+++ Class and Object - 1.java
@@ -0,0 +1,29 @@
+Write a Java program that prints a welcome message and a hello message using two different classes Message and Hello, each with a default constructor that outputs a specific message.
+
+Input Format:
+No console Input.
+
+Output Format:
+The Output Should display the Welcome Message 
+
+Sample 1 Input:
+No input values.
+
+Sample 1 Output:
+Welcome Hello
+
+Code:  
+
+class  print{
+    public static void god(){
+        System.out.println("Welcome Hello");
+    }
+}
+    class Main
+{
+    public static void main(String[] args)
+    {
+        print g=new print();
+       g.god();
+    }
+}
