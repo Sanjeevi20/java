436bd2549f923fdb18ed2080160da5b2540b7631
Sanjeevi20 <136660773+Sanjeevi20@users.noreply.github.com>
2023-07-14 11:17:48 +0530

    Create Print the String 2.java

---
+++ Print the String 2.java
@@ -0,0 +1,26 @@
+Write a program to scan the string from the user and to print it.
+
+Input Format:
+
+Input consist of 1 string.
+Sample Input & Output:
+
+Hello World
+
+The string is Hello World
+
+
+Code:
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String n =sc.nextLine();
+        System.out.println("The string is "+n);
+    }
+}
+
+  
